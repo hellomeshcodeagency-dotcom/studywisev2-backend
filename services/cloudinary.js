5eaf91a43330e503974fa26430035890f07e951e
hellomeshcodeagency-dotcom <hello.meshcodeagency@gmail.com>
2026-08-15 14:32:03 +0100

    Update cloudinary.js

--- services/cloudinary.js
+++ services/cloudinary.js
@@ -1,4 +1,4 @@
-const { v2: cloudinary } = require('cloudinary')
+const cloudinary = require('cloudinary').v2
 const multer = require('multer')
 const { CloudinaryStorage } = require('multer-storage-cloudinary')
 
