a2792d5f02dcfb937101636f093250d6b9dab969
hellomeshcodeagency-dotcom <hello.meshcodeagency@gmail.com>
2026-08-17 05:03:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const seed       = require('./db/seed')
 const app  = express()
 const PORT = process.env.PORT || 5000
 
+app.set('trust proxy', 1)
+
+app.set('trust proxy', 1)
 app.use(cors({ origin: process.env.CLIENT_URL || '*', credentials: true }))
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true }))
